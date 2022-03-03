43dbf42cfb4ddaacdbf13ff5a4e6e02d98312e84
vilmar.junior <vilmar.junior@FLE207D01.alunos.sc.senac.br>
2022-03-03 11:04:26 -0300

    "Esqueleto" dos métodos para EnderecoDAO

--- src/main/java/model/dao/EnderecoDAO.java
+++ src/main/java/model/dao/EnderecoDAO.java
@@ -1,5 +1,47 @@
 package model.dao;
 
+import java.util.ArrayList;
+
+import model.entity.Endereco;
+
 public class EnderecoDAO {
 
+	public Endereco inserir(Endereco novoEndereco) {
+		//TODO implementar
+		
+		return novoEndereco;
+	}
+	
+	public boolean atualizar(Endereco endereco) {
+		boolean atualizou = false;
+		//TODO implementar
+		
+		
+		return atualizou;
+	}
+	
+	public boolean remover(int id) {
+		// DELETE FROM ENDERECO WHERE ID = ?
+		boolean removeu = false;
+		//TODO implementar
+		
+		
+		return removeu;
+	}
+	
+	public Endereco consultar(int id) {
+		Endereco enderecoConsultado = null;
+		//TODO implementar		
+		//SELECT * FROM ENDERECO WHERE ID = ?
+		
+		return enderecoConsultado;
+	}
+	
+	public ArrayList<Endereco> consultarTodos(){
+		ArrayList<Endereco> enderecos = new ArrayList<Endereco>();
+		//TODO implementar
+		//SELECT * FROM ENDERECO
+		
+		return enderecos;
+	}
 }
